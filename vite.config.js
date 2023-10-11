0b171b4f165b0c6ce059f4f01b48963d0184d041
antimatter96 <me.arpitjain09@gmail.com>
2023-10-12 01:15:18 +0800

    feat: improve config
    hide logs
    add extra unique build identifier
    disable consoledebug

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,38 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const commonConfig = {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  build: {
-    assetsDir: 'static',
-    assetsInlineLimit: 1024 * 1024 * 1024,
-    cssCodeSplit: false,
-  },
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true,
+    build: {
+      assetsDir: 'static',
+      assetsInlineLimit: 1024 * 1024 * 1024,
+      cssCodeSplit: false,
     },
-  },
-  envDir: 'configs',
+    server: {
+      host: true,
+      port: 5173,
+      watch: {
+        usePolling: true,
+      },
+    },
+    envDir: 'configs',
+  };
+
+  if (command === 'build' && mode === 'production') {
+    return {
+      ...commonConfig,
+      esbuild: {
+        pure: ['console.debug'],
+      },
+    };
+  }
+  return commonConfig;
 });
