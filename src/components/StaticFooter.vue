1fb58a25e6a128c1fd3d3e12db6d4976c03c5c04
antimatter96 <me.arpitjain09@gmail.com>
2023-10-13 14:05:29 +0800

    feat: componentize components

---
+++ src/components/StaticFooter.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  buildInfo: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+<template>
+  <footer class="footer footer-center py-8 text-base-content bg-slate-400">
+    <aside>
+      <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+      <p v-if="buildInfo != undefined">{{ buildInfo }}</p>
+    </aside>
+  </footer>
+</template>
