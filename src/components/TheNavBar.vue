50f0d8d3fb60742137394a9e10a2a7ca00da19d7
antimatter96 <me.arpitjain09@gmail.com>
2023-10-17 01:59:03 +0800

    feat: improve navbar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -7,18 +7,18 @@ const props = defineProps({
   appName: {
     type: String,
     required: true,
-    value: 'hms',
+    default: 'hms',
   },
   loggedIn: {
     type: Boolean,
     required: false,
-    value: 'false',
+    default: false,
   },
   userInfo: {
     type: Object,
     required: false,
-    default(rawProps) {
-      return { 'fullName': 'Arpit Jain' };
+    default() {
+      return { 'fullName': 'Arpit Jain', 'role': 'admin' };
     },
   },
 });
@@ -31,48 +31,25 @@ const placeHolder = computed(() => {
 
 <template data-theme="corporate">
   <header
-    class="sticky top-0 z-40 py-1 border-b-2 border-b-slate-900/10 backdrop-blur duration-500 supports-[backdrop-blur]:bg-white/95 transition-colors shadow-sm bg-white/95">
-    <div class="w-3/4 max-w-screen-xl mx-auto py-1 navbar text-base-content">
+    class="sticky top-0 z-40 py-1 border-b-2 border-b-slate-900/10 backdrop-blur supports-[backdrop-blur]:bg-white/95 shadow bg-white/95">
+
+    <div class="w-full md:w-3/4 max-w-screen-xl mx-auto pt-1 pb-0 navbar text-base-content">
+
       <div class="navbar-start">
-        <div class="dropdown">
-
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
-          </label>
-          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 w-52">
-            <!-- <li><a>Item 1</a></li> -->
-            <li>
-              <a>Services</a>
-              <ul class="p-2">
-                <li><a>Consultation</a></li>
-                <li><a>Tests</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-
         <RouterLink :to="{ name: 'home' }" class="btn btn-ghost normal-case text-2xl">
           {{ appName }}
         </RouterLink>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li tabindex="0">
-            <details>
-              <summary>Parent</summary>
-              <ul class="p-2 border-solid border-base-content border">
-                <li class="border-solid border-base-content border"><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
+
+      <div class="navbar-center flex">
+        <ul class="menu menu-horizontal text-lg">
+          <li><a>Services</a></li>
+          <!-- <li class="active"><a>Patient Management</a></li> -->
+          <li v-if="userInfo.role == 'admin' || userInfo.role == 'staff'"><a>Patient Management</a></li>
+          <li v-if="userInfo.role == 'admin'"><a>Staff Management</a></li>
         </ul>
       </div>
+
       <div class="navbar-end">
         <template v-if="loggedIn">
           <div class="dropdown dropdown-end ring-0">
@@ -83,7 +60,8 @@ const placeHolder = computed(() => {
                 </div>
               </div>
             </label>
-            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <ul tabindex="0"
+              class="mt-2 z-2 p-0 shadow-2xl menu menu-md border-2 border-base-content dropdown-content bg-base-100 rounded-box w-52">
               <li>
                 <a class="justify-between">
                   Profile
@@ -100,7 +78,6 @@ const placeHolder = computed(() => {
               <li><a>Logout</a></li>
             </ul>
           </div>
-
         </template>
         <template v-else>
           <RouterLink :to="{ name: 'login' }" class="btn">
@@ -108,6 +85,39 @@ const placeHolder = computed(() => {
           </RouterLink>
         </template>
       </div>
+
     </div>
+
   </header>
 </template>
+
+<style>
+.navbar-center {
+  position: relative;
+
+  .menu>li {
+    @apply border-b-2 border-transparent;
+  }
+
+  .menu>li:hover {
+    @apply border-b-2 border-black;
+  }
+
+  .menu>li>a {
+    @apply w-full px-8;
+  }
+
+  .menu>li>a:hover {
+    @apply border-black;
+  }
+
+
+  .menu>li.active {
+    @apply font-bold border-b-2 border-black;
+  }
+}
+
+/* .dropdown-content > li {
+  @apply border-b-2 border-transparent;
+} */
+</style>
