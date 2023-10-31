ef690104c11f4d4d1112714f7064aaa1d46dd88f
Devaki V Devi <e1221828@u.nus.edu>
2023-10-31 14:49:10 +0800

    Introduce constant for user profile api path

--- src/config/apiPaths.js
+++ src/config/apiPaths.js
@@ -8,6 +8,8 @@ const BILL_MANAGEMENT_API_BASE = API_BASE_PATH + '/admin/bills';
 
 const SERVICE_CATALOG_API = API_BASE_PATH + '/available-services';
 
+const USER_PROFILE_API = API_BASE_PATH + '/user/profile';
+
 export {
   API_BASE_PATH,
   ACCOUNTS_API_BASE,
@@ -15,4 +17,5 @@ export {
   SERVICE_CATALOG_API,
 
   BILL_MANAGEMENT_API_BASE,
+  USER_PROFILE_API,
 };
