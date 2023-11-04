8ca03501fd7b255ffaf04ef9cac369e6d2987275
Devaki V Devi <e1221828@u.nus.edu>
2023-11-04 18:18:41 +0800

    feat: Integrate with API to edit user profile

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,5 +1,5 @@
 
-import { easyGet } from './easyFetch';
+import { easyGet, easyPatch } from './easyFetch';
 import { USER_PROFILE_API } from '../config/apiPaths';
 const FILENAME = 'api/serviceCatalog';
 
@@ -12,6 +12,21 @@ export async function fetchUserProfileData() {
     return response.body.data;
   } else {
     console.error(`${FILENAME} - Error fetching user profile`);
+    return null;
+  }
+}
+
+export async function editUserProfileData( { userInfo } ) {
+  const response = await easyPatch({
+    url: USER_PROFILE_API,
+    body: {
+      ...userInfo,
+    },
+  });
+  if (response.done) {
+    console.log(`${FILENAME} - Edited user profile`);
+  } else {
+    console.error(`${FILENAME} - Error editing user profile`);
     return null;
   }
 }
